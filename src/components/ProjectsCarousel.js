b040f4afc9b30144c81d3ac05916e5ab3617fd1b
brunadasilvabz <brunadasilvabz98@gmail.com>
2023-01-20 14:56:54 +0100

    carousel display

--- src/components/ProjectsCarousel.js
+++ src/components/ProjectsCarousel.js
@@ -1,53 +1,53 @@
 import Carousel from 'react-bootstrap/Carousel';
-import "bootstrap/dist/css/bootstrap.css";
+import "../styles/components/Projects.scss";
 
 
 const ProjectsCarousel = () => {
     return (
-        <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-          <Carousel.Item>
-            <div>
-                <button>hi</button>
-                <button>hxfgf</button>
-            </div>
-          </Carousel.Item>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        <Carousel className="carousel">
+            <Carousel.Item className="carousel_div">
+            <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=First slide&bg=373940"
+                alt="First slide"
+            />
+            <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+            <Carousel.Item>
+                <div>
+                    <button>hi</button>
+                    <button>hxfgf</button>
+                </div>
+            </Carousel.Item>
+            </Carousel.Item>
+            <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Second slide&bg=282c34"
+                alt="Second slide"
+            />
+    
+            <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Third slide&bg=20232a"
+                alt="Third slide"
+            />
+    
+            <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                </p>
+            </Carousel.Caption>
+            </Carousel.Item>
       </Carousel>
     );
 }
