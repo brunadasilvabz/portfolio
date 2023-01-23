afe1f5e589d9050e180c04284c3c2a984040eca1
brunadasilvabz <brunadasilvabz98@gmail.com>
2023-01-23 21:11:30 +0100

    add some styles to contactMe

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,21 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 const ContactMe = () => {
     return (
         <section id="contactMe" className="contactSection">
           <h2 className="contactSection_title">contacta conmigo</h2>
-          <p>Puedes ponerte en contacto conmigo en mis redes sociales:</p>
-          <div>
-            <div><a href="">Linkedin</a></div>
-            <div><a href="">Twitter</a></div>
-          </div>
+          
           <div className="contactSection_div">
-            <p>También puedes escribirme directamente un mensaje aquí:</p>
-              <form className="contactSection_form" action="">
-                <legend className="contactSection_form-legend">Nombre completo
-                <input className="contactSection_form-input" type="text"/></legend>
-                <legend className="contactSection_form-legend">Email
-                <input className="contactSection_form-input" type="text"/></legend>
-                <legend className="contactSection_form-legend">Mensaje
-                <input className="contactSection_form-input" type="text"/></legend>
+            <p className="contactSection_div-text">Puedes ponerte en contacto conmigo en mis redes sociales:</p>
+            <div className="contactSection_div-elements">
+              <div className="link_element"><a className="link_element-link" href="">Linkedin</a></div>
+              <div className="link_element"><a className="link_element-link" href="">Twitter</a></div>
+            </div>
+          </div>
+          <div className="contactSection_formDiv">
+            <p className="contactSection_formDiv-text">También puedes escribirme directamente un mensaje aquí:</p>
+              <form className="contactSection_formDiv-form" action="">
+                <legend className="form-legend">Nombre completo
+                <input className="form-input" type="text"/></legend>
+                <legend className="form-legend">Email
+                <input className="form-input" type="text"/></legend>
+                <legend className="form-legend">Mensaje
+                <input className="form-input" type="text"/></legend>
               </form>
           </div>
         </section>
