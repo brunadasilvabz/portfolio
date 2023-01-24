aec406d06c623ac8850ca7f506ee2afa0aa9213b
brunadasilvabz <brunadasilvabz98@gmail.com>
2023-01-24 13:28:14 +0100

    fix about me scss

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,17 +3,22 @@ const AboutMe = () => {
     <section id="aboutMe" className="aboutMeSection">
         <h2 className="aboutMeSection_title">sobre mí</h2>
         <div className="aboutMeSection_div">
-        <img className="aboutMeSection_div-img" src="https://via.placeholder.com/150/" alt="Bruna"/>
-        <p className="aboutMeSection_div-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic totam eligendi
-        atque,
-        veniam culpa magnam
-            accusantium quisquam deleniti. Tempora eaque, doloremque voluptate quae molestiae quas natus qui nihil modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ratione eaque perspiciatis nam nesciunt nulla!
-        Rerum placeat assumenda laudantium, excepturi minus accusamus culpa blanditiis dolorum quaerat inventore
-        veritatis quibusdam amet! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi excepturi, porro
-        omnis beatae cupiditate expedita doloribus nobis labore, harum odit minus iusto ex quas. Sint enim officia
-        maxime impedit laudantium.
-        </p>
+            <div className="aboutMeSection_div-div">
+                <img className="div-img" src="https://via.placeholder.com/150/" alt="Bruna"/>
+                <p className="div-text hidden-div">Tempora eaque, doloremque voluptate quae molestiae quas natus qui nihil modi.</p>
+            </div>
+            <p className="aboutMeSection_div-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic totam eligendi
+            atque,
+            veniam culpa magnam
+                accusantium quisquam deleniti. Tempora eaque, doloremque voluptate quae molestiae quas natus qui nihil modi.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ratione eaque perspiciatis nam nesciunt nulla!
+            Rerum placeat assumenda laudantium, excepturi minus accusamus culpa blanditiis dolorum quaerat inventore
+            veritatis quibusdam amet! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi excepturi, porro
+            omnis beatae cupiditate expedita doloribus nobis labore, harum odit minus iusto ex quas. Sint enim officia
+            maxime impedit laudantium. Eligendi excepturi, porro
+            omnis beatae cupiditate expedita doloribus nobis labore, harum odit minus iusto ex quas. Sint enim officia
+            maxime impedit laudantium.
+            </p>
         </div>
   </section>
   );
